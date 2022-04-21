ca71045a3c1f77cc385caa3103c2e25df4fe2874
billy4479 <giachi.ellero@gmail.com>
2022-04-21 20:17:22 -0230

    Decode inlined images

--- Generator/Generator.cpp
+++ Generator/Generator.cpp
@@ -1,3 +1,5 @@
+#include <stb_image.h>
+
 #include <array>
 #include <filesystem>
 #include <fstream>
@@ -22,34 +24,66 @@ static std::string ToHex(T n) {
 
 int main(int argc, char** argv) {
     std::cout << R"(#include <array>
+#include <glm/ext/vector_int2.hpp>
 #include <string_view>
 
 namespace BillyEngine {
-namespace EngineResources {)";
+namespace EngineResources {
+    
+template <std::size_t S>
+struct Image {
+    std::array<unsigned char, S> Data;
+    glm::ivec2 Size;
+    int BPP;
+};
+)";
 
     for (int i = 1; i < argc; i++) {
         auto name = std::filesystem::path(argv[i]);
         std::cerr << "Bundling " << name.filename() << std::endl;
-
-        std::ifstream current;
-        current.open(name);
-        if (!current.is_open()) return 1;
 
         std::cout << "\n\nstatic constexpr ";
         const auto& n = name.filename().replace_extension().string();
         const auto& ext = name.extension().string();
 
         if (ext == ".txt" || ext == ".glsl") {
-            std::cout << "std::string_view " << n << " = R\"("
-                      << current.rdbuf() << ")\";";
+            std::ifstream f;
+            f.open(name);
+            if (!f.is_open()) return 1;
+
+            std::cout << "std::string_view " << n << " = R\"(" << f.rdbuf()
+                      << ")\";";
+        } else if (ext == ".png" || ext == ".jpg") {
+            int x, y, BPP;
+            const auto* data = stbi_load(name.c_str(), &x, &y, &BPP, 0);
+            if (data == nullptr) return 1;
+            auto size = x * y * BPP;
+
+            std::cout << "Image<" << size << ">" << n << R"( = {
+    .Data = std::array<unsigned char, )"
+                      << size << ">{";
+            for (int i = 0; i < size; i++)
+                std::cout << "0x" << ToHex(data[i]) << ", ";
+
+            std::cout << R"(},
+    .Size = glm::ivec2{)"
+                      << x << ", " << y << R"(},
+    .BPP = )" << BPP << ",\n};\n";
+
+            stbi_image_free((void*)data);
+
         } else {
-            current.seekg(0, std::ios::end);
-            std::cout << "std::array<unsigned char, " << current.tellg() << "> "
-                      << n << " = {";
-            current.seekg(0, std::ios::beg);
+            std::ifstream f;
+            f.open(name);
+            if (!f.is_open()) return 1;
+
+            f.seekg(0, std::ios::end);
+            std::cout << "std::array<unsigned char, " << f.tellg() << "> " << n
+                      << " = {";
+            f.seekg(0, std::ios::beg);
 
             char c;
-            while (current.get(c))
+            while (f.get(c))
                 std::cout << "0x" << ToHex((unsigned char)c) << ", ";
 
             std::cout << "};";
