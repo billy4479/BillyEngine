88671b0bff787fe3f9a0c8d0e0193422f47d7ae5
billy4479 <giachi.ellero@gmail.com>
2022-05-03 23:03:23 -0230

    Fix spdlog defines

--- cmake/Dependencies/spdlog.cmake
+++ cmake/Dependencies/spdlog.cmake
@@ -1,8 +1,9 @@
+set(SPDLOG_NO_EXCEPTIONS ON)
+set(SPDLOG_FMT_EXTERNAL ON)
+
 if(SYSTEM_DEPENDENCIES)
     find_package(spdlog)
 else()
-    set(SPDLOG_NO_EXCEPTIONS ON)
-    set(SPDLOG_FMT_EXTERNAL ON)
     message(STATUS "Getting spdlog ready...")
     FetchContent_Declare(spdlog
         GIT_REPOSITORY "https://github.com/gabime/spdlog"
@@ -10,3 +11,6 @@ else()
     )
     FetchContent_MakeAvailable(spdlog)
 endif()
+
+target_compile_definitions(spdlog::spdlog INTERFACE SPDLOG_NO_EXCEPTIONS)
+target_compile_definitions(spdlog::spdlog INTERFACE SPDLOG_FMT_EXTERNAL)
